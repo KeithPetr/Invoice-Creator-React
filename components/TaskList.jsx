88e3170eb55901a1b57a5d062a821ee95019ab1c
KeithPetr <keith.petryshyn@gmail.com>
2023-08-03 14:33:21 -0400

    reverted to only storing the task names
    able to add the task name to firebase and then have it map over in a list in the UI

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,36 +1,38 @@
 /* eslint-disable react/prop-types */
 import { useState, useRef, useEffect } from "react";
-import { push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  onValue,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 import PlusSign from "../images/plus-sign.svg";
 
-export default function TaskList({tasksDB}) {
+export default function TaskList({ tasksDB }) {
   const [taskList, setTaskList] = useState([]);
   const taskInputRef = useRef(null);
   const dollarAmountRef = useRef(null);
 
   // this function creates a task object that contains the task
-  // name and the amount. It then adds the object to the taskList 
+  // name and the amount. It then adds the object to the taskList
   // array and spreads in previous objects
   function addTask() {
-    const taskObject = {
-        task: taskInputRef.current.value,
-        amount: dollarAmountRef.current.value
-    }
-    setTaskList(prevList => {
-        return [...prevList, taskObject]
-    })
-    taskInputRef.current.value = ''
-    dollarAmountRef.current.value = ''
+    const inputValue = taskInputRef.current.value;
+
+    push(tasksDB, inputValue);
+
+    taskInputRef.current.value = "";
+    dollarAmountRef.current.value = "";
   }
 
-  // Use useEffect to push data to Firebase when taskList changes
   useEffect(() => {
-    if (taskList.length > 0) {
-      push(tasksDB, taskList);
-    }
-  }, [taskList, tasksDB]);
+    // Set up the listener when the component mounts
+    const unsubscribe = onValue(tasksDB, (snapshot) => {
+      const itemsArray = Object.values(snapshot.val());
+      setTaskList(itemsArray);
+    });
 
-  console.log(taskList)
+    // Clean up the listener when the component unmounts
+    return () => unsubscribe();
+  }, [tasksDB]);
 
   return (
     <section className="task-list">
@@ -42,8 +44,16 @@ export default function TaskList({tasksDB}) {
         </button>
       </div>
       <div className="list-headings">
-        <p>Task</p>
-        <p>Task Price</p>
+        <div className="item-names">
+          <p>Task</p>
+          {taskList.map((item) => (
+            <p key={item}>{item}</p>
+          ))}
+        </div>
+        <div className="item-prices">
+          <p>Task Price</p>
+          <div></div>
+        </div>
       </div>
     </section>
   );
